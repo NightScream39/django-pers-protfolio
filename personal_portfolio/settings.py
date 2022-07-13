e78b47a54194573667096e285ef5a6c230c43f92
NightScream39 <roman.nightscream333@gmail.com>
2022-07-13 11:06:14 +0300

    nexttry

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u54j7eoz(k8j+874m@gbumxws$d5$z2k^e$e&1kcrbie$bown%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nightscream28.pythonanywhere.com']
+ALLOWED_HOSTS = ['139.162.172.111:8000']
 
 
 # Application definition
