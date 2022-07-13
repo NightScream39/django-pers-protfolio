db0c6ed5b034ae7582a33259a9abe0de7891d777
NightScream39 <roman.nightscream333@gmail.com>
2022-07-13 12:33:51 +0300

    99 commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u54j7eoz(k8j+874m@gbumxws$d5$z2k^e$e&1kcrbie$bown%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
