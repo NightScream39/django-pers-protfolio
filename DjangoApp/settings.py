bfb995007a6ce2b012b135e58f6798b6d2ae03fc
NightScream39 <roman.nightscream333@gmail.com>
2022-07-13 11:56:56 +0300

    Fixed setting.py

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoApp.urls'
+ROOT_URLCONF = 'personal_portfolio.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DjangoApp.wsgi.application'
+WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 
 # Database
